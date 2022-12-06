319379bbbe6eeea2a252d3c3802f1757cf1fd26d
mark <1210917553@qq.com>
2022-12-06 17:46:57 +0800

    feat: add third section

---
+++ src/components/ThirdSection.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="third-section">
+    <img src="../assets/title1.jpg" alt="title 1">
+    <img class="comment" src="../assets/comment.jpg" alt="comment">
+    <img src="../assets/title2.jpg" alt="title 2">
+    <img class="comment" src="../assets/comment.jpg" alt="comment">
+</div>
+</template>
+
+<style lang="scss" scoped>
+.third-section {
+  img {
+    width: 100%;
+    &.comment {
+      padding: 16px;
+      border-radius: 20px;
+    }
+  }
+}
+</style>
