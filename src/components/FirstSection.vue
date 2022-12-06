e35b5cb68e37b69c77439b072d3a6e6a9df365dd
mark <1210917553@qq.com>
2022-12-06 17:46:25 +0800

    feat: add fitst section

---
+++ src/components/FirstSection.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="first-section">
+    <img src="../assets/banner.jpg" alt="banner" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.first-section {
+  img {
+    width: 100%;
+  }
+}
+</style>
