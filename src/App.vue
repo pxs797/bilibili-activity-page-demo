2338779b97b03f035a37d982768886fe7d8175ed
mark <1210917553@qq.com>
2022-12-06 17:48:06 +0800

    feat: display sections

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import FirstSection from './components/FirstSection.vue';
+import SecondSection from './components/SecondSection.vue';
+import ThirdSection from './components/ThirdSection.vue';
+</script>
+
 <template>
-  <div>hello world</div>
+  <div class="app">
+    <first-section></first-section>
+    <second-section></second-section>
+    <third-section></third-section>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.app {
+  background: #e36c3f;
+  padding-bottom: 64px;
+}
+</style>
